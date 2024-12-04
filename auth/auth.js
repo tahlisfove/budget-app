149c9edfc1afdd022f7ef33d91bdf4db03b14e47
Christoph Samuel <129196750+tahlisfove@users.noreply.github.com>
2024-12-04 15:30:37 +0100

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -46,8 +46,8 @@ app.use(cors({
 
 // Configuration de Passport.js pour l'authentification Google OAuth
 passport.use(new GoogleStrategy({
-  clientID: 209852898924-6ralj3obml7n9p6fffijidb792a7tv69.apps.googleusercontent.com,
-  clientSecret: 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,
+  clientID: "209852898924-6ralj3obml7n9p6fffijidb792a7tv69.apps.googleusercontent.com",
+  clientSecret: "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",
   callbackURL: "http://localhost:3002/auth/google/callback"
 }, (token, tokenSecret, profile, done) => {
   // Fonction appelée après une authentification réussie
